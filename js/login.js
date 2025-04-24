432515d5d8dcc2d2db78329f44a755c4b2c1edaf
sungbly1tsstt <onily.code_sungbly_tsstt@outlook.com>
2025-04-24 11:45:39 -0400

    login v1 /noTurnStile

--- js/login.js
+++ js/login.js
@@ -1,15 +1,42 @@
-function login(account,password,authID,callback) {
-    const accountInfo = $.ajax({
-        url: '//accountsv.838483.xyz/api/login',
+async function login(account,password,authID,callback) {
+    const targetUrl = 'https://accountsv.838483.xyz/api/login';
+    const requestBody = {
+        username: account,
+        password: password,
+        authID: authID
+    };
+    const customHeaders = {
+        'Content-Type': 'application/json'
+    };
+    var accountInfo;
+    await $.ajax({
+        url: '//accountsv.838483.xyz/api/test-request',
         type: 'POST',
-        data: {
-            username: account,
-            password: password,
-            authID: authID
-        },
-        dataType: 'json',
-    }).responseText;
+        data: JSON.stringify({ targetUrl: targetUrl, requestBody: requestBody, headers: customHeaders }),
+        contentType: 'application/json',
+        success: function(data) {
+            accountInfo = data;
+        }
+    }
+    )
+        // const accountInfo = $.ajax({
+        //     url: '//accountsv.838483.xyz/api/login',
+        //     type: 'POST',
+        //     data: JSON.stringify(requestBody),
+        //     contentType: 'application/json'})
     console.log('accountInfo',accountInfo)
+    switch(accountInfo.code){
+        case 401:
+            if(accountInfo.text.substring(0,10)=='Not Allow2'){
+                alert('账户或密码中有字符被拦截')
+            }else{
+                alert('用户名或密码错误')
+            }
+            break;
+        case 200:
+            location.href = window.atob(callback)+'token='+accountInfo.token+'&uid='+accountInfo.uid
+            break;
+    }
 }
 function getLoginInfo(){
     if(location.href.indexOf('?')==-1){
